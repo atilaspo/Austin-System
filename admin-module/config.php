649fee1eeaaf752a38a7f00d028681386cf1af9b
atilaspo <santiago.ortiz@hotmail.com.ar>
2024-10-11 21:56:19 +1100

    Added config.php to manage configuration details

---
+++ admin-module/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$db_password = "";
+$dbname = "admin_module_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $db_password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
